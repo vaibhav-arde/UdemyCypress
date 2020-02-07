434aefaddc5b1a3da59a78287e4b7c989c882cda
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-02-07 11:53:34 +0000

    working with alias

---
+++ cypress/integration/testApp/va.09alias.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress" />
+describe("Perform Login to webapp", ()=>{
+    it("Perform Login", ()=>{
+        cy.visit("http://eaapp.somee.com/");
+
+        // Long way to work with promise using Promise (Closure)
+        cy.get("#loginLink").then((link)=>{
+            return link.text();
+        }).as("loginText");
+        
+        cy.get("@loginText").then((lText)=>{
+            expect(lText).is.eql('Login');
+        });
+        // Shorthand way of working with promise using invoke
+        cy.get("#loginLink").invoke('text').as("iLoginText");
+
+        cy.get("@iLoginText").then((lText)=>{
+            expect(lText).is.eql('Login');
+        });
+    });
+
+    it("Use Alias in another it", ()=>{
+        // Below code fails which shows we can not use one alias in another it()
+        cy.get("@loginText").then((lText)=>{
+            expect(lText).is.eql('Login');
+        });
+    });
+});
