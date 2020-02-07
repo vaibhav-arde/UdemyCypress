51720f3f3e9d895d1819cc4e7010e2fa891a720c
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-02-07 00:06:31 +0000

    Cypress first code

---
+++ cypress/integration/testApp/va.01login.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+describe("Perform Login to webapp", ()=>{
+    it("Perform Login", ()=>{
+        cy.visit("http://eaapp.somee.com/");
+        cy.contains("Login").click();
+        cy.url().should("include", "/Account/Login");
+        cy.get('#UserName').type("admin");
+        cy.get('#Password').type("password");
+        cy.get('.btn').click();
+        
+    });
+})
