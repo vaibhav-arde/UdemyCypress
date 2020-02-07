8aca50430564fb0fca1a75bf816986a022e6bea1
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-02-07 10:44:32 +0000

    working with Table

---
+++ cypress/integration/testApp/va.07handleTable.js
@@ -0,0 +1,17 @@
+/// <reference types="Cypress" />
+describe("Perform Login to webapp", ()=>{
+    it("Perform Login", ()=>{
+        cy.visit("http://eaapp.somee.com/");
+        cy.contains("Login").click();
+        cy.url().should("include", "/Account/Login");
+        cy.get('#UserName').type("admin");
+        cy.get('#Password').type("password");
+        cy.get('.btn').click();
+        cy.contains("Employee List").click();
+        //Below line select all td in the tr
+        cy.get(".table").find("tr > td");
+        cy.get(".table").find("tr").contains("Prashanth").parent().contains("Benefits").click();
+        // cy.get(".table").contains("Prashanth").parent().contains("Benefits")
+        
+    });
+})
